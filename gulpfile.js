b09292aa77cabb409fdb6dbc6fff370080f145b6
Hemalatah <khema2010@gmail.com>
2016-08-17 00:04:10 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+//var browserSync = require('browser-sync').create();
 var eslint = require('gulp-eslint');
 
 
 gulp.task('default', ['styles', 'lint'], function() {
-  // place code for your default task here
   gulp.watch('sass/**/*.scss', ['styles']);
   gulp.watch('js/**/*.js', ['lint']);
-  browserSync.init({
-    server: "./"
-  });
-  
+  //browserSync.init({
+    //server: "./"
+  //});
 });
 
 
-gulp.task('lint', () => {
-    // ESLint ignores files with "node_modules" paths. 
-    // So, it's best to have gulp ignore the directory as well. 
-    // Also, Be sure to return the stream from the task; 
-    // Otherwise, the task may end before the stream has finished. 
-    return gulp.src(['**/*.js','!node_modules/**'])
-        // eslint() attaches the lint output to the "eslint" property 
-        // of the file object so it can be used by other modules. 
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console. 
-        // Alternatively use eslint.formatEach() (see Docs). 
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on 
-        // lint error, return the stream and pipe to failAfterError last. 
-        .pipe(eslint.failAfterError());
-});
 
 gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
@@ -40,4 +22,21 @@ gulp.task('styles', function() {
 			browsers: ['last 2 versions']
 		}))
 		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('lint', function () {
+    // ESLint ignores files with "node_modules" paths. 
+    // So, it's best to have gulp ignore the directory as well. 
+    // Also, Be sure to return the stream from the task; 
+    // Otherwise, the task may end before the stream has finished. 
+    return gulp.src(['js/**/*.js'])
+        // eslint() attaches the lint output to the "eslint" property 
+        // of the file object so it can be used by other modules. 
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console. 
+        // Alternatively use eslint.formatEach() (see Docs). 
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on 
+        // lint error, return the stream and pipe to failAfterError last. 
+        .pipe(eslint.failOnError());
 });
